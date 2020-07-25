87f3f60c0757be0701529d3a2db656ce277a0279
wolffdy <wolffdy0@gmail.com>
2020-07-25 22:10:22 +0200

    e1.1

--- Main.hs
+++ Main.hs
@@ -2,3 +2,25 @@ module Main where
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev 0 = [0]
+toDigitsRev i = toDigitsRevRec i
+
+toDigitsRevRec :: Integer -> [Integer]
+toDigitsRevRec 0 = []
+toDigitsRevRec i = let car = i `mod` 10 in
+             let dividend = i `div` 10 in
+             car:toDigitsRevRec dividend
+
+toDigits :: Integer -> [Integer]
+toDigits 0 = [0]
+toDigits i = toDigitsRec i
+
+toDigitsRec :: Integer -> [Integer]
+toDigitsRec 0 = []
+toDigitsRec i = let car = i `mod` 10 in
+             let dividend = i `div` 10 in
+             (toDigitsRec dividend) ++ [car]
+
+
