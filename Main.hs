31919dcac31f7dd5b748aa70a440e6f927d3dda5
wolffdy <wolffdy0@gmail.com>
2020-07-26 12:59:10 +0200

    e1.5

--- Main.hs
+++ Main.hs
@@ -32,3 +32,11 @@ sumDigits digs = sum $ digs >>= toDigits
 
 validate :: Integer -> Bool
 validate x = (== 0) $ flip mod 10 $ sumDigits $ doubleEveryOther $ toDigits x
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 1 a b _ = [(a, b)]
+hanoi n a b c = hanoi (n - 1) a c b
+            ++ hanoi 1 a b c
+            ++ hanoi (n - 1) c b a
